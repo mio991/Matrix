1337eb2640dcd9b4036ffa1750b6b144dc3e6567
Jonas Arndt <mio991@gmx.de>
2017-01-05 12:02:07 +0100

    Die Funktion: void set(size_t m, double value) zu Vector2 hinzugefügt.

--- Vector2.cpp
+++ Vector2.cpp
@@ -6,3 +6,8 @@ Vector2::Vector2(double a1, double a2) : Matrix(2, 1)
 	at(0) = a1;
 	at(1) = a2;
 }
+
+Vector2::set(size_t m, double value)
+{
+	set(m, 0, value);
+}
--- Vector2.hpp
+++ Vector2.hpp
@@ -7,4 +7,6 @@ class Vector2 : public Matrix
 public:
 	Vector2();
 	Vector2(double a1, double a2);
+
+	void set(size_t m, double value);
 };
