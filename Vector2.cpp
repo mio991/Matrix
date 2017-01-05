4378b2a5794b1905bdb4799cd2485318313e88d2
Jonas Arndt <mio991@gmx.de>
2017-01-05 12:08:44 +0100

    Bugfix missing void

--- Vector2.cpp
+++ Vector2.cpp
@@ -7,7 +7,7 @@ Vector2::Vector2(double a1, double a2) : Matrix(2, 1)
 	at(1) = a2;
 }
 
-Vector2::set(size_t m, double value)
+void Vector2::set(size_t m, double value)
 {
-	set(m, 0, value);
+	Matrix::set(m, 0, value);
 }
